3739f5a6ad6ca929728ff11a7e2c03cbc434bfed
Monica Lisa Reyes <mlisareyes@gmail.com>
2018-07-22 03:37:50 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,25 @@
 require 'open-uri'
 require 'pry'
+require 'nokogiri'
 
 class Scraper
-
   def self.scrape_index_page(index_url)
-    
+    doc = Nokogiri::HTML(open(index_url))
+    doc.css(".student-card").map do |student|
+      {
+      name: student.css("h4").text,
+      location: student.css("p").text,
+      profile_url: student.css("a")[0]["href"]
+    }
+    end
   end
 
   def self.scrape_profile_page(profile_url)
+    doc = Nokogiri::HTML(open(profile_url))
+    profile = {}
+    doc.css(".social-icon-container").each do |link|
     
+
   end
 
 end
-
