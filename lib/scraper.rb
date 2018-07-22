a1ce7e682e5f9b689b21b0ddb52bd0799d5efbb3
Monica Lisa Reyes <mlisareyes@gmail.com>
2018-07-22 03:49:52 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -18,6 +18,7 @@ class Scraper
     doc = Nokogiri::HTML(open(profile_url))
     profile = {}
     doc.css(".social-icon-container").each do |link|
+      url = link.attribute("href").value
       if link.include?("linkedin")
         student[:linkedin] = link
       elsif link.include?("github")
